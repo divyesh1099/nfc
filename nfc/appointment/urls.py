832803562cdba86156fcfa0da4739c91e27bf838
Divyesh Vishwakarma <divyesh1099@gmail.com>
2023-11-05 21:03:19 +0530

    Add Appointment Feature

--- nfc/appointment/urls.py
+++ nfc/appointment/urls.py
@@ -4,7 +4,7 @@ app_name='appointment'
 urlpatterns=[
     path('', views.index, name='index'),
     path('<int:appointment_id>', views.appointment_detail, name='appointment_detail'),
-    path('update/<int:appointment_id>', views.appointment_detail, name='appointment_update'),
-    path('cancel/<int:appointment_id>', views.appointment_detail, name='appointment_cancel'),
+    path('update/<int:pk>', views.appointment_update, name='appointment_update'),
+    path('cancel/', views.appointment_cancel, name='appointment_cancel'),
     path('schedule_appointment', views.schedule_appointment, name='schedule_appointment'),
 ]
--- nfc/appointment/views.py
+++ nfc/appointment/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from doctor.models import *
 from patient.models import *
@@ -7,6 +7,8 @@ from .forms import *
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from datetime import timedelta
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 def index(request):
     user=request.user
@@ -54,3 +56,37 @@ def appointment_detail(request, appointment_id):
         'appointment': appointment,
     }
     return render(request, 'appointment/appointment_detail.html', context)
+
+@login_required
+def appointment_update(request, pk):
+    appointment = get_object_or_404(Appointment, pk=pk)
+    
+    # Security check to ensure the logged-in user is associated with the appointment
+    if appointment.patient.user != request.user:
+        messages.error(request, "You do not have permission to edit this appointment.")
+        print(appointment.patient.user, request.user)
+        return redirect('appointment:index')
+
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST, instance=appointment)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your appointment has been updated!')
+            return redirect('appointment:index')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = AppointmentForm(instance=appointment)
+        form.fields['doctor'].queryset = Doctor.objects.all()
+
+    return render(request, 'appointment/appointment_update.html', {'form': form, 'appointment': appointment, 'doctors': Doctor.objects.all()})
+
+def appointment_cancel(request):
+    appointment_id = request.GET.get('id')
+    # Add checks here to ensure the logged-in user has the right to cancel this appointment
+    
+    # Perform the cancellation logic
+    Appointment.objects.filter(id=appointment_id).update(status='Cancelled')  # Or however you manage status
+
+    messages.success(request, 'The appointment has been cancelled.')
+    return HttpResponseRedirect(reverse('appointment:index'))  # Redirect to the appointments page or wherever appropriate
